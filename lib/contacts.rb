7adedd61ddf16e4154411d6b7d1394f4eac06ce5
Julie  McManus <julieannesays@gmail.com>
2020-01-28 22:24:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,8 +16,7 @@ require 'pry'
 def remove_strawberry(hash)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-
-    contact_details_hash.each do |attribute, data|
+      contact_details_hash.each do |attribute, data|
       binding.pry
     end
   end
